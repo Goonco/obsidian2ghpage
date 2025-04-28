7608a77d6f69a21a2fc68c3f7518cac50cb17f26
송경호 <s07019@naver.com>
2025-04-28 16:11:53 +0900

    feat: add file tree extraction

---
+++ scripts/dump-mdx.ts
@@ -0,0 +1,61 @@
+import fs from "fs";
+import path from "path";
+import generateFileTree, { SRC_DIR } from "./file-tree.ts";
+import type { FileSystemNode } from "./file-tree.ts";
+
+const DEST_DIR = path.resolve("./blog");
+
+function ensureDir(dir: string) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+}
+
+function walk(fsNode: FileSystemNode, pathAcc: string) {
+  if (fsNode.type === "file") {
+    const { name, path: filePath } = fsNode;
+    const newContent = modifyContent(fs.readFileSync(filePath, "utf-8"));
+    fs.writeFileSync(path.join(pathAcc, `${name}.mdx`), newContent);
+  }
+
+  if (fsNode.type === "directory") {
+    const { name, children } = fsNode;
+    const walkingDir = path.join(pathAcc, name);
+    ensureDir(walkingDir);
+    children?.forEach((node) => walk(node, walkingDir));
+  }
+}
+
+function modifyContent(content: string): string {
+  return content;
+}
+
+function main() {
+  const fileTree = generateFileTree("", SRC_DIR);
+  if (fileTree) {
+    walk(fileTree, DEST_DIR);
+    fs.writeFileSync(
+      path.join(DEST_DIR, "file-tree.json"),
+      JSON.stringify(fileTree, null, 2)
+    );
+  }
+}
+
+main();
+
+// Helper function to log the file tree
+function recursiveLog(value: any, depth = 0) {
+  const indent = "  ".repeat(depth);
+
+  if (typeof value === "object" && value !== null) {
+    console.log(`${indent}{`);
+    for (let key in value) {
+      if (value.hasOwnProperty(key)) {
+        if (typeof value[key] === "object" && value[key] !== null)
+          recursiveLog(value[key], depth + 1);
+        else console.log(`${indent}  ${key}: ${value[key]}`);
+      }
+    }
+    console.log(`${indent}}`);
+  }
+}
---
+++ scripts/file-tree.ts
@@ -0,0 +1,59 @@
+import fs from "fs";
+import ignore from "ignore";
+import path from "path";
+
+export const SRC_DIR = path.resolve("./obsidian");
+export const IGNORE_FILE = path.resolve(SRC_DIR, ".mdxignore");
+
+const ign = ignore().add(fs.readFileSync(IGNORE_FILE, "utf-8").toString());
+
+type FileNode = {
+  type: "file";
+  path: string;
+  name: string;
+};
+
+type DirectoryNode = {
+  type: "directory";
+  name: string;
+  children: FileSystemNode[] | null;
+};
+
+export type FileSystemNode = FileNode | DirectoryNode;
+
+export default function generateFileTree(
+  dirName: string,
+  absPath: string
+): FileSystemNode | null {
+  const currentDirectory: FileSystemNode = {
+    type: "directory",
+    name: dirName,
+
+    children: [],
+  };
+
+  fs.readdirSync(absPath, { withFileTypes: true }).forEach((entry) => {
+    const currentPath = path.join(absPath, entry.name);
+
+    if (
+      entry.isDirectory() &&
+      !ign.ignores(path.relative(SRC_DIR, currentPath))
+    ) {
+      const childDir = generateFileTree(entry.name, `${absPath}/${entry.name}`);
+      if (childDir) currentDirectory.children!.push(childDir);
+    }
+
+    if (
+      entry.isFile() &&
+      entry.name.endsWith(".md") &&
+      !ign.ignores(path.relative(SRC_DIR, currentPath))
+    )
+      currentDirectory.children!.push({
+        type: "file",
+        path: currentPath,
+        name: entry.name.replace(/\.md$/, ""),
+      });
+  });
+
+  return currentDirectory.children!.length > 0 ? currentDirectory : null;
+}
