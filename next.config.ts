2becd6291ba073655ba90778a80027ba7bb63ffe
송경호 <s07019@naver.com>
2025-04-30 00:26:21 +0900

    feat: add links and add github styling

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,6 +9,10 @@ const nextConfig = {
 };
 
 const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
   // Add markdown plugins here, as desired
   extension: /\.(md|mdx)$/,
 });
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,25 +5,47 @@ export default function Home() {
   const fileTree = fetchFileTree();
 
   if (fileTree.type === "directory")
-    return <Directory directoryNode={fileTree} />;
-  else return <File fileNode={fileTree} />;
+    return <Directory directoryNode={fileTree} path="blog" />;
+  else return <File fileNode={fileTree} path="blog" />;
 }
 
-export function Directory({ directoryNode }: { directoryNode: DirectoryNode }) {
+export function Directory({
+  directoryNode,
+  path,
+}: {
+  directoryNode: DirectoryNode;
+  path: string;
+}) {
   return (
     <div>
       <h2 className="text-2xl">{directoryNode.name}</h2>
       {directoryNode.children?.map((child) => {
         if (child.type === "directory") {
-          return <Directory key={child.name} directoryNode={child} />;
+          return (
+            <Directory
+              key={child.name}
+              directoryNode={child}
+              path={`${path}/${directoryNode.name}`}
+            />
+          );
         } else {
-          return <File key={child.path} fileNode={child} />;
+          return (
+            <File
+              key={child.path}
+              fileNode={child}
+              path={`${path}/${directoryNode.name}`}
+            />
+          );
         }
       })}
     </div>
   );
 }
 
-export function File({ fileNode }: { fileNode: FileNode }) {
-  return <div className="text-sm">{fileNode.name}</div>;
+export function File({ fileNode, path }: { fileNode: FileNode; path: string }) {
+  return (
+    <a href={encodeURI(`${path}/${fileNode.name}`)} className="text-sm">
+      {fileNode.name}
+    </a>
+  );
 }
